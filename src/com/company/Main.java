73fe0cd88e01f6a1225fd9b8ba9ff7dd0f177bff
“Yuri <yura.kopshev@gmail.com>
2021-05-09 08:55:55 +0000

    added java files

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import static com.company.Worker.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+
+        Worker worker = new Worker(listener,errorListener);
+        worker.start();
+    }
+
+}
